84722eebd1a2fdbce4d31624d207571537472075
Sander Star <sanderstar1@gmail.com>
2017-11-09 11:16:05 +0100

    Aanpassen inkomend bericht

--- src/android/GeoInt.java
+++ src/android/GeoInt.java
@@ -24,7 +24,7 @@ public class GeoInt extends CordovaPlugin {
 
     private void coolMethod(String message, CallbackContext callbackContext) {
         if (message != null && message.length() > 0) {
-            callbackContext.success(message);
+            callbackContext.success("Hello" + message);
         } else {
             callbackContext.error("Expected one non-empty string argument.");
         }
