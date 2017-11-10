2c69a76fe27afe7500be0a8bf5051b843263e3d7
Sander Star <sanderstar1@gmail.com>
2017-11-10 15:04:17 +0100

    toevoegen get locatie aan javascript api

--- www/GeoInt.js
+++ www/GeoInt.js
@@ -3,3 +3,7 @@ var exec = require('cordova/exec');
 exports.coolMethod = function (arg0, success, error) {
     exec(success, error, 'GeoInt', 'coolMethod', [arg0]);
 };
+
+exports.getLocation = function (arg0, success, error) {
+    exec(success, error, 'GeoInt', 'getLocation', [arg0]);
+};
