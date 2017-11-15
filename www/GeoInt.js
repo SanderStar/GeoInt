aa8779316afda0d9e350efb7448eb4557920783a
Sander Star <sanderstar1@gmail.com>
2017-11-15 16:22:53 +0100

    add stop location

--- www/GeoInt.js
+++ www/GeoInt.js
@@ -1,9 +1,13 @@
 var exec = require('cordova/exec');
 
-exports.coolMethod = function (arg0, success, error) {
+exports.coolMethod = function(arg0, success, error) {
     exec(success, error, 'GeoInt', 'coolMethod', [arg0]);
 };
 
-exports.getLocation = function (success, error) {
+exports.getLocation = function(success, error) {
     exec(success, error, 'GeoInt', 'getLocation');
 };
+
+exports.stopLocation = function(succes, error) {
+    exec(success, error, 'GeoInt', 'stopLocation');
+}
