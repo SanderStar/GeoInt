ffb6f0d9d8912eead65fb992dda4483e7ad520a9
Sander Star <sanderstar1@gmail.com>
2017-11-29 15:30:44 +0100

    adjust js interface (watch / clear)

--- www/GeoInt.js
+++ www/GeoInt.js
@@ -1,4 +1,7 @@
-var exec = require('cordova/exec');
+var exec = require('cordova/exec'),
+    utils = require('cordova/utils'),
+    argscheck = require('cordova/argscheck'),
+    timers = {};
 
 exports.coolMethod = function(arg0, success, error) {
     exec(success, error, 'GeoInt', 'coolMethod', [arg0]);
@@ -10,6 +13,30 @@ exports.getLocation = function(success, error) {
 
 exports.stopLocation = function(success, error) {
     exec(success, error, 'GeoInt', 'stopLocation');
+};
+
+exports.getCurrentSensor = function(success, error) {
+    exec(success, error, 'GeoInt', 'getCurrentSensor');
+};
+
+exports.watchCurrentSensor = function(success, error) {
+  // Default interval (100 msec)
+  var frequency = (options !== undefined && options.frequency !== undefined) ? options.frequency : 100;
+
+  var id = utils.createUUID();
+  timers[id] = window.setInterval(function () {
+    fusion.getCurrentSensorFusion(successCallback, errorCallback);
+  }, frequency);
+
+  return id;
+};
+
+exports.clearWatch = function(id) {
+  // Stop javascript timer & remove from timer list
+  if (id && timers[id]) {
+    clearInterval(timers[id]);
+    delete timers[id];
+  }
 };
 
 exports.startSensor = function(success, error) {
